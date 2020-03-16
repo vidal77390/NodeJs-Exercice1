49798be831df03bbc5ef3e29e4270e65ed2a0d00
VIDAL Léo <vidalleo@MacBook-Pro-de-VIDAL.local>
2020-03-16 16:05:57 +0100

    exercice4

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 var express = require("express");
 var app = express();
+app.use(express.json())
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
  console.log("Server running on port 3000");
 });
-
 app.get("", (req, res, next) => {
   res.json(["Hello World"]);
 });
-
-/*app.get("/hello?:nom", (req, res, next) => {
-  res.json(["Quel est votre nom ?"]);
-});*/
-
-
 app.get("/hello", (req, res, next) => {
-  if(req.query.nom){
-    res.json(["Bonjour, ", req.query.nom, " !"]);
-  }else{
-    res.json(["Quel est votre nom ?"]);
-  }
+  if (req.query.nom) { res.json(["Bonjour, ", req.query.nom, " !"]); }
+  else{ res.json(["Quel est votre nom ?"]); }
+});
+app.post("/chat", function(req , res){
+  if(req.body.msg == "ville") { res.json(["Nous sommes à Paris"]); }
+  else if(req.body.msg == "météo") { res.json(["Il fait beau"]); }
 });
