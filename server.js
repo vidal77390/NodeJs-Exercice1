c776c06ce26cc52d7b3c6446d7f576097808b1db
VIDAL Léo <vidalleo@MacBook-Pro-de-VIDAL.local>
2020-03-16 14:55:22 +0100

    changement port heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
  console.log("Server running on port 3000");
 });
 
