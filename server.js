f4c96febbd088538402f92888c624b49a7483472
VIDAL Léo <vidalleo@macbook-pro-de-vidal.home>
2020-03-17 12:11:03 +0100

    exercice 5

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
+var fs = require('fs');
 var app = express();
 app.use(express.json())
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port 3000 !");
 });
 app.get("", (req, res, next) => {
   res.json(["Hello World"]);
@@ -13,7 +14,35 @@ app.get("/hello", (req, res, next) => {
   if (req.query.nom) { res.json(["Bonjour, ", req.query.nom, " !"]); }
   else{ res.json(["Quel est votre nom ?"]); }
 });
+
 app.post("/chat", function(req , res){
-  if(req.body.msg == "ville") { res.json(["Nous sommes à Paris"]); }
-  else if(req.body.msg == "météo") { res.json(["Il fait beau"]); }
+  if(req.body.msg == "ville") {
+    res.json(["Nous sommes à Paris"]);
+  }
+  else if(req.body.msg == "météo"){
+     res.json(["Il fait beau"]);
+  }else{
+    if(req.body.msg.includes('=')){
+      const word = req.body.msg.split("=");
+      const keyValue = "{" + word[0] + ": " + word[1] + "}";
+      console.log("write msg");
+      fs.writeFileSync("/Users/vidalleo/Documents/ESGI/NodeJs/Exercice1/NodeJs-Exercice1/reponse.json", keyValue); // écris hello dans le fichier response.json
+      res.json(["Merci pour cette information !"]);
+    }else{
+      var contentFile = fs.readFileSync('/Users/vidalleo/Documents/ESGI/NodeJs/Exercice1/NodeJs-Exercice1/reponse.json', 'utf8');
+      console.log("fichier : ",contentFile);
+      if(contentFile.includes(req.body.msg)){
+        console.log("retourne la key value");
+        const response = contentFile.substring(1, contentFile.length - 1);
+        res.json([response]);
+      }else{
+        console.log("retourne rien");
+        const response = "Je ne connais pas " + req.body.msg + " ...";
+        res.json([response]);
+      }
+
+    }
+
+
+  }
 });
